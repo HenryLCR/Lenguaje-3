0ffcae22fc3771867e2953c643a0e6232cdfa6cc
Henry La chira <Henrylachira1995@gmail.com>
2021-05-31 22:30:08 -0600

    agregando1

---
+++ Ventas/Ventas/Formlogin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ventas
+{
+    public partial class Formlogin : Form
+    {
+        public Formlogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Piensa, cree, sueña y atrevete");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
