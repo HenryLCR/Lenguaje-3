aade4a1875d15149cfb0a11b09594669a4bbe139
Henry La Chira <henrylachira1995@gmail.com>
2021-07-11 14:53:02 -0600

    Añadiendo Tarea parte 5

--- Sistema de Salon de belleza/SeguridadBL/ProductosBL.cs
+++ Sistema de Salon de belleza/SeguridadBL/ProductosBL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,67 +10,38 @@ namespace BL.Rentas
 {
    public  class ProductosBL
     {
+        Contexto _contexto; //instanciar variable contexto
         public BindingList<Producto> ListaProductos { get; set;  }
 
         public ProductosBL()
-        
-            
-
         {
+            _contexto = new Contexto(); //instanciar en el contructor bl
             ListaProductos = new BindingList<Producto>();
-
-            var Producto1 = new Producto();
-            Producto1.Id = 001;
-            Producto1.Descripcion = "Plancha de Ceramica";
-            Producto1.Marca = "Remington S9500 Pearl";
-            Producto1.Precio = 5000;
-            Producto1.Existencia = 5;
-            Producto1.Activo = true;
-
-            ListaProductos.Add(Producto1);
-
-            var Producto2 = new Producto();
-            Producto2.Id = 002;
-            Producto2.Descripcion = "Delantal para estilistas.";
-            Producto2.Marca = "LCL Beauty ";
-            Producto2.Precio = 50;
-            Producto2.Existencia = 100;
-            Producto2.Activo = true;
-
-            ListaProductos.Add(Producto2);
-
-            var Producto3 = new Producto();
-            Producto3.Id = 003;
-            Producto3.Descripcion = "Sillón reclinable hidráulico.";
-            Producto3.Marca = "Heavy Duty  ";
-            Producto3.Precio = 1500;
-            Producto3.Existencia = 4;
-            Producto3.Activo = true;
-
-            ListaProductos.Add(Producto3);
+            
         }
         public BindingList<Producto> ObtenerProductos()
         {
+            _contexto.Productos.Load();
+            ListaProductos = _contexto.Productos.Local.ToBindingList();
+
             return ListaProductos;
         }
 
         public Resultado GuardarProducto(Producto producto)
         {
             var resultado = Validar(producto);
-            if(resultado.Exitoso == false)
+            if (resultado.Exitoso == false)
             {
                 return resultado;
             }
-                
-            if (producto.Id == 0)
-            {
-                producto.Id = ListaProductos.Max(item => item.Id) + 1;
-            }
+
+            _contexto.SaveChanges();
+
             resultado.Exitoso = true;
             return resultado;
         }
-       
-            public void AgregarProducto()
+
+        public void AgregarProducto()
         {
             var nuevoProducto = new Producto();
             ListaProductos.Add(nuevoProducto);
@@ -77,12 +49,13 @@ namespace BL.Rentas
 
         public bool EliminarProducto(int id)
         {
-            foreach (var producto in ListaProductos)
+            foreach (var producto in ListaProductos )
             {
-                if (producto.Id == id)
+                if (producto.Id==id)
                 {
                     ListaProductos.Remove(producto);
-                        return true;
+                    _contexto.SaveChanges();
+                    return true;
                 }
             }
 
@@ -92,16 +65,17 @@ namespace BL.Rentas
 
         private Resultado Validar(Producto producto)
         {
+
             var resultado = new Resultado();
             resultado.Exitoso = true;
-            
-            if(string.IsNullOrEmpty(producto.Descripcion)== true)
+
+            if (string.IsNullOrEmpty(producto.Descripcion)==true)
             {
-                resultado.Mensaje = "Ingrese una descripcion";
+                resultado.Mensaje = "Ingrese una descripción";
                 resultado.Exitoso = false;
             }
 
-            if (producto.Existencia < 0)
+            if(producto.Existencia<0)
             {
                 resultado.Mensaje = "La existencia debe ser mayor que cero";
                 resultado.Exitoso = false;
@@ -114,7 +88,9 @@ namespace BL.Rentas
             }
 
             return resultado;
+
         }
+
     }
 public class Producto
     {
@@ -125,10 +101,14 @@ public class Producto
         public int Existencia { get; set; }
         public bool Activo { get; set; }
     }
+
+    public class Resultado
+    {
+        public bool Exitoso { get; set; }
+        public string Mensaje { get; set; }
+    }
+
 }
 
-public class Resultado
-{
-    public bool Exitoso { get; set; }
-    public string Mensaje { get; set; }
-}
+
+
